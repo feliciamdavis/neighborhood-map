12b677d95253c4f3dac0481c2064e8d8f3c0e7e7
Felicia Davis <davisfeliciam@gmail.com>
2018-11-20 16:28:45 -0800

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+export class App extends React.Component {
+
+    render() {
+        return (
+            <div className="app">
+            HI OMG CHROME NEEDS DARK MODE
+            MY EYES!!!! They BURN!!!
+            </div>
+        )
+    }
+
+}
