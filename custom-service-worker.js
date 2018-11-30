47210bc0f88ebba95124e03e4934677865f32737
Felicia Davis <davisfeliciam@gmail.com>
2018-11-29 19:51:14 -0800

    Updates

--- custom-service-worker.js
+++ custom-service-worker.js
@@ -6,7 +6,6 @@
 
 /** List of all the things */
 const urlsToCache = [
-    'https://feliciamdavis.github.io/neighborhood-map/',
     'https://fonts.googleapis.com/css?family=Roboto:400,700',
     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
@@ -32,23 +31,30 @@ self.addEventListener('install', () => {
 })
 
 self.addEventListener('fetch', event => {
-    console.log(`Fetch event for ${event.request.url}`)
+    let url = event.request.url
+
+    // add index.html if no file was given so we can find it in the cache
+    if (url.endsWith('/')) {
+        url += 'index.html'
+    }
+
+    console.log(`Fetch event for ${url}`)
     event.respondWith(
         caches.open(staticCacheName).then(cache => {
-            return cache.match(event.request.url)
+            return cache.match(url)
                 .then(response => {
                     if (response) {
-                        console.log(`Found cached response for ${event.request.url}`)
+                        console.log(`Found cached response for ${url}`)
                         return response
                     }
-                    throw new Error(`Found cached response is empty for ${event.request.url}`)
+                    throw new Error(`Found cached response is empty for ${url}`)
                 })
                 .catch(() => {
-                    console.log(`Network request for ${event.request.url}`)
+                    console.log(`Network request for ${url}`)
                     return fetch(event.request)
                         .then(response => {
-                            console.log(`Caching response for ${event.request.url}`)
-                            cache.put(event.request.url, response.clone())
+                            console.log(`Caching response for ${url}`)
+                            cache.put(url, response.clone())
                             return response
                         })
                 })
